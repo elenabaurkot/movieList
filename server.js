8694baea881be461f5c310da4ff3f8cfe7740a96
elenabaurkot <elenabaurkot@gmail.com>
2021-03-29 01:08:51 -0400

    server file created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+// const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Express middleware set up
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(routes);
+
+// Handlebars setup
+app.engine("handlebars", exphbs({ defaultLayout: "main " }));
+app.set("view engine", "handlebars");
+
+// Tell server to listen
+app.listen(PORT, () => console.log(`App is listening on localhost:${PORT}`));
