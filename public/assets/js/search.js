38d057e002987ee1495457c80cdaecbaeda53f86
elenabaurkot <elenabaurkot@gmail.com>
2021-04-18 00:29:19 -0400

    spacing and scroll added to movie cards

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -7,17 +7,17 @@ const appendMovies = (results) => {
   let movieDiv = document.getElementById("movie-div");
   console.log(results);
   if (results.message) {
-    movieDiv.innerHTML = `<h4 class="mt-5 text-center">${results.message}</h4>`;
+    movieDiv.innerHTML = `<h4 class="text-center">${results.message}</h4>`;
   } else if (results.length != 0) {
     let movieHTML = "";
     results.forEach((movie) => {
-      movieHTML += `<div class="card m-2" style="width: 200px;">
+      movieHTML += `<div class="movie-card card overflow-auto" style="width: 220px;">
       <img src= "https://image.tmdb.org/t/p/w200${movie.poster_path}" class="card-img-top" alt="${movie.title} cover image">
       <div class="card-body">
         <h5 class="card-title">${movie.title}</h5>
-        <p class="card-text">${movie.overview}</p>
         <a href="#" class="btn btn-primary mb-1">Add to Watch List</a>
         <a href="#" class="btn btn-primary">Add to Favorites</a>
+        <p class="card-text">${movie.overview}</p>
       </div>
     </div>`;
     });
